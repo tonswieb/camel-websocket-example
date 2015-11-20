88f4f8ce2f7887714f54e64ac2f263f65871e3e9
Ton Swieb <ton.swieb@finalist.nl>
2015-11-20 14:26:21 +0100

    Initial commit

---
+++ src/main/java/nl/finalist/test/ws/WebSocketRoute.java
@@ -0,0 +1,36 @@
+package nl.finalist.test.ws;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class WebSocketRoute extends RouteBuilder {
+
+	public final static String WEBSOCKET_BROADCAST_URI = "websocket://camel-tweet?sendToAll=true&staticResources=classpath:webapp";
+	public final static String WEBSOCKET_URI = "websocket://camel-tweet?staticResources=classpath:webapp";
+
+	private ConcurrentHashMap<String, String> activeClients = new ConcurrentHashMap<>();
+	
+	@Override
+	public void configure() throws Exception {
+		
+		//Generate event every 6 seconds
+		from("timer://foo?fixedRate=true&period=6000")
+		.setBody(constant("Broadcast message"))
+		.to(WEBSOCKET_BROADCAST_URI);
+		
+		from(WEBSOCKET_URI)
+		.log("Received: ${body} from ${header[websocket.connectionKey]}")
+		.bean(activeClients,"put(${header[websocket.connectionKey]},Test)")
+		.setBody(simple("${body} from ${header[websocket.connectionKey]}"))
+		.to(WEBSOCKET_URI);
+		
+		from("timer://client?fixedRate=true&period=6000")
+		.setBody(constant(activeClients.keySet()))
+		.split(body())
+			.setHeader("websocket.connectionKey",body())
+			.log("Test message to: ${body}")
+			.setBody(simple("Test message to: ${body}"))
+			.to(WEBSOCKET_URI);
+	}
+}

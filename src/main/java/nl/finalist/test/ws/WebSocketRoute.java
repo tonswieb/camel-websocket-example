4886e6df06d9b6a024b7fb8edbb55bccbfd048ca
Ton Swieb <ton.swieb@finalist.nl>
2015-11-21 23:13:29 +0100

    Keep all uri's the same as a workaround for https://issues.apache.org/jira/browse/CAMEL-9348

--- src/main/java/nl/finalist/test/ws/WebSocketRoute.java
+++ src/main/java/nl/finalist/test/ws/WebSocketRoute.java
@@ -3,10 +3,10 @@ package nl.finalist.test.ws;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.websocket.WebsocketConstants;
 
 public class WebSocketRoute extends RouteBuilder {
 
-	public final static String WEBSOCKET_BROADCAST_URI = "websocket://camel-tweet?sendToAll=true&staticResources=classpath:webapp";
 	public final static String WEBSOCKET_URI = "websocket://camel-tweet?staticResources=classpath:webapp";
 
 	private ConcurrentHashMap<String, String> activeClients = new ConcurrentHashMap<>();
@@ -14,23 +14,25 @@ public class WebSocketRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		
-		//Generate event every 6 seconds
-		from("timer://foo?fixedRate=true&period=6000")
-		.setBody(constant("Broadcast message"))
-		.to(WEBSOCKET_BROADCAST_URI);
-		
-		from(WEBSOCKET_URI)
+		from(WEBSOCKET_URI)	
 		.log("Received: ${body} from ${header[websocket.connectionKey]}")
+		.setHeader(WebsocketConstants.SEND_TO_ALL,constant(""))
 //		.bean(activeClients,"put(${header[websocket.connectionKey]},'Test')")
 		.setBody(simple("${body} from ${header[websocket.connectionKey]}"))
 		.to(WEBSOCKET_URI);
+
+		//Generate event every 6 seconds
+		from("timer://foo?fixedRate=true&period=6000")
+		.setBody(constant("Broadcast message"))
+		.setHeader(WebsocketConstants.SEND_TO_ALL,constant(true))
+		.to(WEBSOCKET_URI);		
 		
-//		from("timer://client?fixedRate=true&period=6000")
-//		.setBody(constant(activeClients.keySet()))
-//		.split(body())
-//			.setHeader("websocket.connectionKey",body())
-//			.log("Test message to: ${body}")
-//			.setBody(simple("Test message to: ${body}"))
-//			.to(WEBSOCKET_URI);
+		from("timer://client?fixedRate=true&period=6000")
+		.setBody(constant(activeClients.keySet()))
+		.split(body())
+			.setHeader("websocket.connectionKey",body())
+			.log("Test message to: ${body}")
+			.setBody(simple("Test message to: ${body}"))
+			.to(WEBSOCKET_URI);
 	}
 }
